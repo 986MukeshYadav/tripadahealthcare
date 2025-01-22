8186af5e2e1c7e718efed3d952307a952ccc8967
Mukesh <myadav986@rku.ac.in>
2025-01-22 19:32:40 +0530

    upto gallery section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
+// tailwind.config.js
 module.exports = {
-    theme: {
-      extend: {
-        fontFamily: {
-          oxygen: ['Oxygen', 'sans-serif'],
-        },
+  content: [
+    "./index.html", 
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        oxygen: ['Oxygen', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif']
+      },
+      fontSize: {
+        'sm': '14px', // You can modify font-size here too if needed
       },
     },
-  }
-  
+  },
+  plugins: [],
+}
